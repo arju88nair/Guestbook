3932ff8bfb47848a001765149968740147ce5dcb
arju88nair <arju88nair@gmail.com>
2019-01-29 21:16:01 +0530

    initial

---
+++ Controllers/UserController.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Copyright (C) Covalense Technologies - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Nair, 29/1/19 8:41 PM
+ */
+
+namespace Controllers;
+
+class UserController
+{
+
+}
